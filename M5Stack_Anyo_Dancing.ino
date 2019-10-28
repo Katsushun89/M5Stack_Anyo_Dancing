a6715cd12bf590338335494f35a61e87691b3b12
Katsushun89 <Katsushun89@gmail.com>
2019-10-29 00:18:30 +0900

    Print received data.

--- M5Stack_Anyo_Dancing.ino
+++ M5Stack_Anyo_Dancing.ino
@@ -31,10 +31,13 @@ void configDeviceAP()
 
 void onDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
   char macStr[18];
-  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
-           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-  Serial.print("Last Packet Recv from: "); Serial.println(macStr);
-  Serial.print("Last Packet Recv Data: "); Serial.println(*data);
+  //snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+  //         mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  //Serial.print("Last Packet Recv from: "); Serial.println(macStr);
+  Serial.print("Recv Data: ");
+  for(uint32_t i = 0; i < data_len; i++){
+    Serial.print(""); Serial.print(data[i]);
+  }
   Serial.println("");
 }
 
